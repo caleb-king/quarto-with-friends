f6815cba9bf6e7cd42fe91c7ed3b6bca3c606f0f
Caleb King <caleb.king536@gmail.com>
2020-08-24 13:34:48 -0600

    adds landing page content and styling

--- src/landing-page/LandingPage.js
+++ src/landing-page/LandingPage.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function LandingPage() {
   return (
-    <p>Landing Page</p>
+    <>
+      <header>
+        <img className="logo" src="/images/quarto-logo.png" alt="Quarto Logo"/>
+        <h1>
+          Quarto With Friends
+        </h1>
+      </header>
+      <main>
+        <p>Quarto is a modern classic strategy game using pieces which combine 4 attributes – size, color, shape, and consistency:</p>
+        <img className="comparing-attributes" src="/images/attribute-comparison.png" alt="comparison of dichotomous attributes" />
+        <p>The goal is to place the fourth piece in a row or diagonal where each piece shares one attribute in common.</p>
+        <p>The twist is that your opponent chooses the piece you place on the board each turn.</p>
+        <div className="button-container">
+          <Link to='/setup' className="link-button">
+            START A GAME
+          </Link>
+        </div>
+      </main>
+    </>
   );
 }
 
