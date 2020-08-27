1444c9f5d76f9472fbb8aaf06db77fbe717f02ab
Caleb King <caleb.king536@gmail.com>
2020-08-27 09:10:25 -0600

    updates 'BASE_URL'

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
-const API_ENDPOINT = 'http://localhost:8000';
+// const API_ENDPOINT = 'http://localhost:8000';
+const API_ENDPOINT = 'https://obscure-harbor-97497.herokuapp.com'
 
-const BASE_URL = 'http://localhost:3000'
+// const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'https://quarto-with-friends.vercel.app'
 
 export {
   API_ENDPOINT,
